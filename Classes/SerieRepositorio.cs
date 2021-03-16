bdf6413c37fc4944fda4daa8a7def2181a2778dc
Hugo Flausino <hugo.flausino@hotmail.com>
2021-03-16 08:19:40 -0300

    Create project

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using dio_series.Interfaces;
+
+namespace dio_series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            listaSerie[id] = entidade;
+        }
+
+        public void Excluir(int id)
+        {
+           listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            listaSerie.Add(entidade);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
